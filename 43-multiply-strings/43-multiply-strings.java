36b25d984cb2efa9b6d9321817cf244a741cb802
ShubhamBhardwaj001 <88887600+ShubhamBhardwaj001@users.noreply.github.com>
2022-08-30 15:44:19 +0530

    Time: 4 ms (97.43%), Space: 41.2 MB (96.10%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,16 +1,16 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int nums[]=new int[num1.length()+num2.length()];  //number of digit in answer is <=num1.length+num2.legth
+        int nums[]=new int[num1.length()+num2.length()];  
         if(num1.equals("0") || num2.equals("0"))return "0";
         for(int i=num1.length()-1;i>=0;i--){
             for(int j=num2.length()-1;j>=0;j--){
                 nums[i+j+1]+=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
-                nums[i+j]+=nums[i+j+1]/10 ; //carry
-                    nums[i+j+1]=nums[i+j+1]%10;  //digit;
+                nums[i+j]+=nums[i+j+1]/10 ; 
+                    nums[i+j+1]=nums[i+j+1]%10; 
             }
         }
         int i=0;
-        while(i<nums.length && nums[i]==0)i++; //if answer<num1.leng+num2.len  then in starting in array there are some 0
+        while(i<nums.length && nums[i]==0)i++; 
         StringBuilder st=new StringBuilder("");
         while(i<nums.length){
             st.append(nums[i]);
